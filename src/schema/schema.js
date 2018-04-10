f3ae1bab16223e420acdfa9bd236a0027444851a
platypusrex <frankcooke79@gmail.com>
2018-04-10 18:40:51 -0400

    Rework - adding addtional type defs for Board and Suggestion, and query and mutation types for Users, Boards, and Suggestions

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,19 +1,48 @@
 export const typeDefs = `
+	type Suggestion {
+		id: Int!
+		text: String!
+		userId: Int!
+		boardId: Int!
+		createdAt: String!
+		updatedAt: String!
+		username: String!
+	}
+
+	type Board {
+		id: Int!
+		name: String!
+		userId: Int!
+		createdAt: String!
+		updatedAt: String!
+		suggestions: [Suggestion!]
+	}
+
 	type User {
 		id: Int!
 		username: String!
 		createdAt: String!
 		updatedAt: String!
+		boards: [Board!]!
+		suggestions: [Suggestion!]!
 	}
 	
 	type Query {
 		allUsers: [User!]!
+		allBoards: [Board!]!
+		allSuggestions: [Suggestion!]!
 		getUser(username: String!): User
+		userBoards(userId: Int!): [Board!]!
+		userSuggestions(userId: Int!): [Suggestion!]!
 	}
 	
 	type Mutation {
 		createUser(username: String!): User
 		updateUser(username: String!, newUsername: String!): [Int!]!
 		deleteUser(username: String!): Int!
+		createBoard(userId: Int!, name: String!): Board!
+		deleteBoard(id: Int!): Int!
+		createSuggestion(userId: Int!, boardId: Int!, text: String!): Suggestion!
+		deleteSuggestion(id: Int!): Int!
 	}
 `;
