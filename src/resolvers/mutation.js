d4ff85f493fd103b5ae606175d0414e50b10ea6d
platypusrex <frankcooke79@gmail.com>
2018-04-11 10:05:45 -0400

    Rework - adds resolvers for register and login

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,20 +1,44 @@
+import {createToken, hashPassword, validatePassword} from "../utils/authUtil";
+
 export default {
 	Mutation: {
 		// User
-		createUser: (parent, args, {User}) => User.create(args),
-		updateUser: (parent, {username, newUsername}, {User}) =>
-			User.update(
+		createUser: (parent, args, {models}) => models.User.create(args),
+		register: async (parent, user, {models}) => {
+			user.password = await hashPassword(user.password);
+
+			return models.User.create(user);
+		},
+		login: async (parent, {email, password}, {models}) => {
+			const user = await models.User.findOne({where: {email}});
+
+			if (!user) {
+				throw new Error('User not found');
+			}
+
+			const valid = await validatePassword(password, user.password);
+
+			if (!valid) {
+				throw new Error('Email or password incorrect');
+			}
+
+			const token = createToken(user);
+
+			return token;
+		},
+		updateUser: (parent, {username, newUsername}, {models}) =>
+			models.User.update(
 				{ username: newUsername },
 				{ where: { username } }
 			),
-		deleteUser: (parent, args, {User}) => User.destroy({where: args}),
+		deleteUser: (parent, args, {models}) => models.User.destroy({where: args}),
 
 		// Board
-		createBoard: (parent, args, {Board}) => Board.create(args),
-		deleteBoard: (parent, args, {Board}) => Board.destroy({where: args}),
+		createBoard: (parent, args, {models}) => models.Board.create(args),
+		deleteBoard: (parent, args, {models}) => models.Board.destroy({where: args}),
 
 		// Suggestion
-		createSuggestion: (parent, args, {Suggestion}) => Suggestion.create(args),
-		deleteSuggestion: (parent, args, {Suggestion}) => Suggestion.destroy(args)
+		createSuggestion: (parent, args, {models}) => models.Suggestion.create(args),
+		deleteSuggestion: (parent, args, {models}) => models.Suggestion.destroy(args)
 	}
 };
