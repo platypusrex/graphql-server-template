e5f1892a28218f0a63af21bb5d74fe2d02156627
platypusrex <frankcooke79@gmail.com>
2018-04-11 10:07:56 -0400

    Rework - adds cors middleware and also verify token middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import { typeDefs } from './schema/schema';
 import { resolvers } from './resolvers';
 import models from './models';
+import { port } from "./utils/config";
+import { verifyToken } from "./utils/authUtil";
 
 const schema = makeExecutableSchema({
 	typeDefs,
@@ -13,16 +16,17 @@ const schema = makeExecutableSchema({
 
 const app = express();
 
-app.use(
-	'/graphiql',
-	graphiqlExpress({
-		endpointURL: '/graphql',
-	}),
-);
-
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: models }));
+app
+	.use(cors('*'))
+  .use(verifyToken)
+	.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+	.use(
+		'/graphql',
+		bodyParser.json(),
+		graphqlExpress(req => ({ schema, context: {models, user: req.user} }))
+	);
 
 models.sequelize.sync()
 	.then(() =>
-		app.listen(8000, () => console.log('listening on port 8000'))
+		app.listen(port, () => console.log('listening on port 8000'))
 	);
